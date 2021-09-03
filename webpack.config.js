ab29ae4f7bab9a4309a281ec06fd4c207d77974d
你çxuCheng <1390683299@qq.com>
2021-09-03 08:04:56 +0800

    updata

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const { mode } = require('./webpack.config');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
   output: {
-    filename: 'index.[contenthash].js'
+    filename: mode === 'production' ? '[name].[chunkhash].js' : '[name].[hash].js'
   },
   plugins: [ new HtmlWebpackPlugin({
     title: 'XMDL - 写代码啦',
@@ -15,7 +20,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   }
